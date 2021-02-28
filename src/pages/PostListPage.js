38f17c3792b45adb663f99cc889dfead20e44f8c
kitaiki <giae2527@naver.com>
2021-02-28 23:08:30 +0900

    init project

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostListPage(props) {
+  return <div>PostListPage</div>;
+}
+
+export default PostListPage;
---
+++ src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WritePage(props) {
+  return <div>WritePage</div>;
+}
+
+export default WritePage;
